66f74c79bd038275cbd9f6b8b0f70849c2e7b94a
Justin Lam <jlam123321@gmail.com>
2025-02-10 18:42:02 -0800

    added object pooling for asteroids
    still needs to test destroy

--- Scripts/Obstacles/Asteroid.cs
+++ Scripts/Obstacles/Asteroid.cs
@@ -3,26 +3,30 @@ using System;
 
 /*
  * Author: [Lam, Justin]
- * Last Updated: [1/30/25]
+ * Last Updated: [02/10/2025]
  * [scirpt for the player]
  */
 
-public partial class Asteroid : Area2D
+public partial class Asteroid : Area2D, IPoolItem
 {
-    [Export] private float _velocityRange = 150f;
+    [Export] private float _velocityRangeMin = 100f;
+    [Export] private float _velocityRangeMax = 150f;
     [Export] private float _screenWarpBuffer = 50f;
 
     //velocity
     private Vector2 _velocity = Vector2.Zero;
+
+    public bool active { get; set; } = false;
 
     /// <summary>
     /// on start, set velocity
     /// </summary>
     public override void _Ready()
     {
-        _velocity = new Vector2(
-            (float)GD.RandRange(-_velocityRange, _velocityRange),
-            (float)GD.RandRange(-_velocityRange, _velocityRange));
+        Visible = false;
+        Monitoring = false;
+        Monitorable = false;
+        SetProcess(false);
     }
 
     /// <summary>
@@ -67,5 +71,35 @@ public partial class Asteroid : Area2D
         {
             Position = new Vector2(Position.X, -_screenWarpBuffer);
         }
+    }
+
+    /// <summary>
+    /// when spawned for object pooling
+    /// </summary>
+    public void SpawnFromPool()
+    {
+        active = true;
+        Visible = true;
+        Monitoring = true;
+        Monitorable = true;
+        SetProcess(true);
+
+        _velocity = new Vector2(
+            (float)GD.RandRange(-1, 1),
+            (float)GD.RandRange(-1, 1));
+
+        _velocity = _velocity.Normalized() * (float)GD.RandRange(_velocityRangeMin, _velocityRangeMax);
+    }
+
+    /// <summary>
+    /// deactivates the object for pooling
+    /// </summary>
+    public void ReturnToPool()
+    {
+        active = false;
+        Visible = false;
+        SetDeferred(Area2D.PropertyName.Monitoring, false);
+        SetDeferred(Area2D.PropertyName.Monitorable, false);
+        SetProcess(false);
     }
 }
