946d1b8dcc86de9e2a76646d3a07625cada60307
Justin Lam <jlam123321@gmail.com>
2025-02-25 15:10:46 -0800

    Missile explodes

--- Scripts/Player/Missile/MissileTriggerHitbox.cs
+++ Scripts/Player/Missile/MissileTriggerHitbox.cs
@@ -1,11 +1,23 @@
 using Godot;
 using System;
+using System.Reflection;
+
+/*
+ * Author: [Lam, Justin]
+ * Last Updated: [02/25/2025]
+ * [Hitbox for missile]
+ */
 
 public partial class MissileTriggerHitbox : Area2D
 {
     [Export] private Missile _missile;
+    [Export] private PackedScene _explosion;
     [Export] private int _onHitDamage = 1;
 
+    /// <summary>
+    /// hitbox for missle to damage whatever it hits
+    /// </summary>
+    /// <param name="area">other</param>
     private void OnAreaEntered(Area2D area)
     {
         if (area is HitboxComponent)
@@ -15,14 +27,26 @@ public partial class MissileTriggerHitbox : Area2D
             hitbox.Damage(_onHitDamage);
 
             //explosion
+            CallDeferred("AddExplosion");
 
             _missile.QueueFree();
         }
     }
 
+    /// <summary>
+    /// creates explosion
+    /// </summary>
+    private void AddExplosion()
+    {
+        Node2D explosion = _explosion.Instantiate<Node2D>() as Node2D;
+        explosion.GlobalPosition = _missile.GlobalPosition;
+        explosion.GlobalRotation = _missile.GlobalRotation;
+        GetTree().Root.AddChild(explosion);
+    }
+
     private void OnArmed()
     {
-        Monitoring = true;
-        Monitorable = true;
+        SetDeferred(Area2D.PropertyName.Monitoring, true);
+        SetDeferred(Area2D.PropertyName.Monitorable, true);
     }
 }
