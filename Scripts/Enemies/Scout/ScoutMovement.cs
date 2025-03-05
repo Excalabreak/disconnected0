e12f251fc16fa49307ab00ebe0b84fd7cf2a8b1e
Justin Lam <jlam123321@gmail.com>
2025-03-05 15:35:21 -0800

    added basic components to scout

---
+++ Scripts/Enemies/Scout/ScoutMovement.cs
@@ -0,0 +1,30 @@
+using Godot;
+using System;
+
+/*
+ * Author: [Lam, Justin]
+ * Last Updated: [03/05/2025]
+ * [Scout Movement pattern]
+ */
+
+public partial class ScoutMovement : BaseEnemyMovement
+{
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+
+    /// <summary>
+    /// calls to move
+    /// </summary>
+    /// <param name="delta"></param>
+    public override void Move(float delta)
+    {
+
+    }
+}
