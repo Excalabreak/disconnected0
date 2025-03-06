5e0282a45c406f5f2d8545286a7a7fd6a7c40624
Justin Lam <jlam123321@gmail.com>
2025-03-05 20:09:49 -0800

    scanning state

--- Scripts/Enemies/Scout/States/ScoutDeployState.cs
+++ Scripts/Enemies/Scout/States/ScoutDeployState.cs
@@ -9,13 +9,18 @@ using System;
 
 public partial class ScoutDeployState : State
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-	}
+    public override void Ready()
+    {
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
+    }
+
+    public override void Enter()
+    {
+
+    }
+
+    public override void Exit()
+    {
+
+    }
 }
--- Scripts/Enemies/Scout/States/ScoutScanState.cs
+++ Scripts/Enemies/Scout/States/ScoutScanState.cs
@@ -1,5 +1,6 @@
 using Godot;
 using System;
+using System.Diagnostics;
 
 /*
  * Author: [Lam, Justin]
@@ -9,13 +10,94 @@ using System;
 
 public partial class ScoutScanState : State
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-	}
+    [Export] private Sprite2D[] _signalSprites;
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
+    [Export] private Area2D _scanArea;
+
+    [Export] private Sprite2D _lightSprite;
+    [Export] private Color _scoutColor;
+
+    [Export] private Timer _scanTimer;
+    [Export] private float _scanTime = 3f;
+
+    /// <summary>
+    /// on ready
+    /// set scanner to false
+    /// set timer
+    /// </summary>
+    public override void Ready()
+    {
+        SetScanner(false);
+
+        _scanTimer.WaitTime = _scanTime;
+        _scanTimer.OneShot = true;
+    }
+
+    /// <summary>
+    /// on enter:
+    /// set color of sprite
+    /// activate scanner
+    /// start scan timer
+    /// </summary>
+    public override void Enter()
+    {
+        _lightSprite.Modulate = _scoutColor;
+
+        SetScanner(true);
+
+        _scanTimer.Start();
+    }
+
+    /// <summary>
+    /// when exiting state
+    /// set scanner to false
+    /// stop timer
+    /// </summary>
+    public override void Exit()
+    {
+        SetScanner(false);
+
+        _scanTimer.Stop();
+    }
+
+    /// <summary>
+    /// sets the scanner on or off
+    /// </summary>
+    /// <param name="isScanning">is the scanner on</param>
+    private void SetScanner(bool isScanning)
+    {
+        foreach (Sprite2D sprite in _signalSprites)
+        {
+            sprite.Visible = isScanning;
+        }
+
+        if (isScanning)
+        {
+            _scanArea.Monitoring = true;
+        }
+        else
+        {
+            _scanArea.SetDeferred(Area2D.PropertyName.Monitoring, isScanning);
+        }
+    }
+
+    /// <summary>
+    /// if timer times out
+    /// go back to wander state
+    /// </summary>
+    private void OnTimerTimeout()
+    {
+        sm.TransitionTo("WanderState");
+    }
+
+    /// <summary>
+    /// if player is in area,
+    /// go to deploy state
+    /// </summary>
+    /// <param name="area"></param>
+    private void OnAreaEntered(Area2D area)
+    {
+        //switch state to deploy
+        GD.Print("deploy");
+    }
 }
